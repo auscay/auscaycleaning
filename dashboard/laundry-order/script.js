9cc16ce922028a93f974fe4aaaafcab806220277
Auscay <ebowusifeanyi@gmail.com>
2022-12-11 09:58:16 -0600

    added laundry order form

---
+++ dashboard/laundry-order/script.js
@@ -0,0 +1,129 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+var output_1 = document.getElementById("demo1");
+output.innerHTML = slider.value;
+output_1.innerHTML = slider.value * 100;
+slider.oninput = function() {
+output.innerHTML = this.value;
+output_1.innerHTML = this.value * 100;
+}
+
+var slider2 = document.getElementById("myRange2");
+var output2 = document.getElementById("demo2");
+var output_2 = document.getElementById("demo22");
+output2.innerHTML = slider2.value;
+output_2.innerHTML = slider2.value * 100;
+slider2.oninput = function() {
+output2.innerHTML = this.value;
+output_2.innerHTML = this.value * 100;
+}
+
+var slider3 = document.getElementById("myRange3");
+var output3 = document.getElementById("demo3");
+var output_3 = document.getElementById("demo33");
+output3.innerHTML = slider3.value;
+output_3.innerHTML = slider3.value * 80;
+slider3.oninput = function() {
+output3.innerHTML = this.value;
+output_3.innerHTML = this.value * 80;
+}
+
+var slider4 = document.getElementById("myRange4");
+var output4 = document.getElementById("demo4");
+var output_4 = document.getElementById("demo44");
+output4.innerHTML = slider4.value;
+output_4.innerHTML = slider4.value * 50;
+slider4.oninput = function() {
+output4.innerHTML = this.value;
+output_4.innerHTML = this.value * 50;
+}
+
+var slider5 = document.getElementById("myRange5");
+var output5 = document.getElementById("demo5");
+var output_5 = document.getElementById("demo55");
+output5.innerHTML = slider5.value;
+output_5.innerHTML = slider5.value * 100;
+slider5.oninput = function() {
+output5.innerHTML = this.value;
+output_5.innerHTML = this.value * 100;
+}
+
+var slider6 = document.getElementById("myRange6");
+var output6 = document.getElementById("demo6");
+var output_6 = document.getElementById("demo66");
+output6.innerHTML = slider6.value;
+output_6.innerHTML = slider6.value * 200;
+slider6.oninput = function() {
+output6.innerHTML = this.value;
+output_6.innerHTML = this.value * 200;
+}
+
+var slider7 = document.getElementById("myRange7");
+var output7 = document.getElementById("demo7");
+var output_7 = document.getElementById("demo77");
+output7.innerHTML = slider7.value;
+output_7.innerHTML = slider7.value * 700;
+slider7.oninput = function() {
+output7.innerHTML = this.value;
+output_7.innerHTML = this.value * 700;
+}
+
+var slider8 = document.getElementById("myRange8");
+var output8 = document.getElementById("demo8");
+var output_8 = document.getElementById("demo88");
+output8.innerHTML = slider8.value;
+output_8.innerHTML = slider8.value * 400;
+slider8.oninput = function() {
+output8.innerHTML = this.value;
+output_8.innerHTML = this.value * 400;
+}
+
+var slider9 = document.getElementById("myRange9");
+var output9 = document.getElementById("demo9");
+var output_9 = document.getElementById("demo99");
+output9.innerHTML = slider9.value;
+output_9.innerHTML = slider9.value * 600;
+slider9.oninput = function() {
+output9.innerHTML = this.value;
+output_9.innerHTML = this.value * 600;
+}
+
+var slider10 = document.getElementById("myRange10");
+var output10 = document.getElementById("demo10");
+var output_10 = document.getElementById("demo100");
+output10.innerHTML = slider10.value;
+output_10.innerHTML = slider10.value * 600;
+slider10.oninput = function() {
+output10.innerHTML = this.value;
+output_10.innerHTML = this.value * 600;
+}
+
+var slider11 = document.getElementById("myRange11");
+var output11 = document.getElementById("demo11");
+var output_11 = document.getElementById("demo111");
+output11.innerHTML = slider11.value;
+output_11.innerHTML = slider11.value * 300;
+slider11.oninput = function() {
+output11.innerHTML = this.value;
+output_11.innerHTML = this.value * 300;
+}
+
+var slider12 = document.getElementById("myRange12");
+var output12 = document.getElementById("demo12");
+var output_12 = document.getElementById("demo112");
+output12.innerHTML = slider12.value;
+output_12.innerHTML = slider12.value * 300;
+slider12.oninput = function() {
+output12.innerHTML = this.value;
+output_12.innerHTML = this.value * 300;
+}
+
+var slider13 = document.getElementById("myRange13");
+var output13 = document.getElementById("demo13");
+var output_13 = document.getElementById("demo113");
+output13.innerHTML = slider13.value;
+output_13.innerHTML = slider13.value * 2800;
+slider13.oninput = function() {
+output13.innerHTML = this.value;
+output_13.innerHTML = this.value * 2800;
+}
